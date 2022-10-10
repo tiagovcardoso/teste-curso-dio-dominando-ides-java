04863e9139738301c88ba1c299ffb329d255e1c3
tiagovcardoso <tiagovcardoso@gmail.com>
2022-10-10 15:14:33 -0300

    update

--- src/com/dio/calculadora/Calcuradora.java
+++ src/com/dio/calculadora/Calcuradora.java
@@ -9,18 +9,20 @@ public class Calcuradora {
 
 		Scanner scan = new Scanner(System.in);
 		
-		int a, b;
+		double a,b;
+		
 		
 		System.out.println("Digite o primeiro valor: ");
 		a = scan.nextInt();
 		System.out.println("Digite o segundo valor: ");
 		b = scan.nextInt();
 		
-		int soma = soma(a,b);
-		int subtracao = subtracao(a,b);
-		int multiplicacao = multiplicacao(a,b);
-		int divisao = divisao(a,b);
-	
+		double soma = soma(a,b);
+		double subtracao = subtracao(a,b);
+		double multiplicacao = multiplicacao(a,b);
+		double divisao = divisao(a,b);
+		
+		
 		System.out.println("soma " + soma);
 		System.out.println("sub " + subtracao);
 		System.out.println("mult " + multiplicacao);
@@ -29,16 +31,16 @@ public class Calcuradora {
 	}
 	
 	
-	public static int soma(int a, int b) {
+	public static double soma(double a, double b) {
 		return a + b;
 	}
-	public static int subtracao(int a, int b) {
+	public static double subtracao(double a, double b) {
 		return a - b;
 	}
-	public static int divisao(int a, int b) {
+	public static double divisao(double a, double b) {
 		return a / b;
 	}
-	public static int multiplicacao(int a, int b) {
+	public static double multiplicacao(double a, double b) {
 		return a * b;
 	}
 	
